73f8b8d37d32639b05c897342aed7c9d39b0463a
fraserdscott <fraserdscott@gmail.com>
2022-04-06 17:17:34 +0100

    Add back context

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
   import {chainTempo} from '$lib/blockchain/chainTempo';
   import {wallet} from '$lib/blockchain/wallet';
+
+  import {context} from '$lib/plugin/plugin';
   
   chainTempo.startOrUpdateProvider(wallet.provider);
 </script>
 
-  <p class="text-lg font-bold">GM</p>
-  <p>I hope you're having a great day :)</p>
-  <p>More plugins to come!</p>
-  <p class="p-2">- fraserd.eth</p>
+{#if $context.initialized}
+    <p class="text-lg font-bold">GM</p>
+    <p>I hope you're having a great day :)</p>
+    <p>More plugins to come!</p>
+    <p class="p-2">- fraserd.eth</p>
+{:else}
+  <p>Waiting for initialization to complete....</p>
+{/if}
